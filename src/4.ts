3e9e6b380c6db196c67e1107720d67bd0013337a
Nanovik <127130547+Nanovik@users.noreply.github.com>
2023-12-10 00:02:56 +0200

    HW - 7
    add hw-7

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,57 @@
+class Key {
+  private signature = Math.random()
+
+  getSignature() {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+  constructor(key: Key) {
+    this.key = key;
+  }
+  getKey() {
+    return this.key;
+  }
+}
+
+class House {
+  tenants: Person[] = [];
+
+  constructor(public door: boolean, public key: Key) {
+    this.door = door;
+  }
+  
+  comeIn(person: Person): void {
+    if (this.door === true) {
+      this.tenants.push(person);
+      console.log('Welcome!');
+    } else {
+      console.log('The door is closed.');
+    }
+  }
+
+  openDoor(key: Key): void {
+    this.key = key;
+  }
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super(false, key);
+  }
+
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("The door is now open.");
+    } else {
+      console.log("The door is closed.");
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -8,4 +62,4 @@ house.openDoor(person.getKey());
 house.comeIn(person);
 
 
-export {};
+// export {};
